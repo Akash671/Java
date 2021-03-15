6ba637e8fbdd8eee834ef9983a8b3039adbc4f72
Akash671 <akashsaini454545gmail.com>
2021-03-15 03:00:12 -0400

    logical and '

---
+++ logicaland.java
@@ -0,0 +1,20 @@
+/* author akash kaumr */
+
+
+import java.util.*;
+public class logicaland
+{
+    public static void main(String args[])
+    {
+        //Scanner sc=new Scanner(Systaem.in);
+        Scanner sc=new Scanner(System.in); 
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0;
+        if(a==10 && b==10)
+        {
+            c=1;
+        }
+        System.out.println("result is :"+c);
+    }
+}
